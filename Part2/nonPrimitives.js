700ac3a0c0bfcab1dd94e8397dc905082d08bd69
chiragbudhbaware <chiragbudhbaware@gmail.com>
2026-07-12 16:49:17 +0530

    Condition Challenges

---
+++ Part2/nonPrimitives.js
@@ -0,0 +1,32 @@
+let username = {
+    firstname : "Chirag",
+    isLoggedIn : true
+};
+
+// username.firstname = "Mr.C"
+// username.lastname = "Budhbaware"
+
+// console.log(username.firstname);
+// console.log(username.lastname);
+
+// console.log(username);
+
+
+// console.log(typeof(username));
+
+// let today = new Date();
+// console.log(today);
+
+// Array 
+
+let anotherUser = ["Chirag", true]; // Can be stores multiple values
+// console.log(anotherUser[0]);
+
+// console.log(1 + 2 + "1");
+
+let isValue = "3";
+console.log(Number(isValue));
+
+
+
+
---
+++ Part3/condition-challenges.js
@@ -0,0 +1,63 @@
+// 1. Checking if number is greater than another or not ?
+
+// let num1 = 3;
+// let num2 = 5;
+
+// if (num1 > num2) {
+//     console.log(num2," is greater than ",num1);
+    
+// } else {
+//     console.log(num1," is greater than ",num2);
+// }
+
+// 2. Check if username is same or not
+
+// let username  = "Chirag";
+// let anotherUsername = "Chirag";
+
+// if(username == anotherUsername){
+//     console.log("Username is same");
+    
+// } else{
+//     console.log("Username is not same");
+    
+// }
+
+
+// 3. Check if a variable is number or not ?
+
+// let variable = 3;
+
+// if(typeof variable === 'number'){
+//     console.log("Given variable is number");
+    
+// }else{
+//     console.log("Given variable is not number");
+    
+// }
+
+
+// 4. Check if a boolean value is true of false:
+
+// let isLoggedIn = false;
+
+// if(isLoggedIn){
+//     console.log("True");
+    
+// }else{
+//     console.log("False");
+    
+// }
+
+
+// Checking if array is empty or not:
+
+let array = [1,2,3];
+
+if (array.length === 0) {
+    console.log("Empty");
+    
+} else {
+    console.log("NOT Empty");
+    
+}
