b6cd19b17a07d935d050559e18aff4119b639bf8
chiragbudhbaware <chiragbudhbaware@gmail.com>
2026-07-13 09:25:50 +0530

    Array challenges

---
+++ Part4/array_challenges.js
@@ -0,0 +1,30 @@
+// 1. Declare an array name 'teaFlavors' that contains the String
+// "green tea", "black tea" and "oolong tea".
+//     Acces the first element of the array and store  it 
+//     in a variable named 'firstTea'.
+
+let teaFlavors = ['green tea', 'black tea', 'oolong tea'];
+
+let firstTea = teaFlavors[0];
+
+// console.log(firstTea);
+
+// 2. Declare an array named 'cities' containing
+// 'London', 'Tokyo', 'Paris' and 'New York'.
+// Acces the third element in the array and store it 
+// in a variable named 'favoriteCity'.
+
+let cities = ['London', 'Tokyo', 'Paris', 'New York'];
+let favoriteCity = cities[2];
+
+// console.log(favoriteCity);
+
+// 3. You have an array named 'teaTypes' containing
+//  'herbal tea', 'white tea' and 'masala chai'.
+//   Change the second element of the array to 'jasmine tea'. 
+
+let teaTypes = ['herbal tea','white tea','masala chai'];
+
+teaTypes[1] = "jasmine tea";
+
+console.log(teaTypes);
