4f3014276739fa10ec47810172899338aa599792
chiragbudhbaware <chiragbudhbaware@gmail.com>
2026-07-21 00:21:30 +0530

    Day-7

---
+++ Functions/ArrowFunctions.js
@@ -0,0 +1,15 @@
+// ARROW FUNCTIONS
+
+// 1. Syntax 
+
+const sayHello = () => {
+    console.log("Hey!");
+};
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const addV2 = (num1, num2) => num1 + num2;  // This is One-Liner approach
+
+console.log(addV2(3,4));
--- Functions/functions.js
+++ Functions/functions.js
@@ -14,8 +14,39 @@ function add(num1, num2){  // Here num1 and num2 are the arguments
 
 // Functions for multiplication
 
-function prod(num1 , num2){
-    console.log(num1 * num2);
+// function prod(num1 , num2){
+//     console.log(num1 * num2); // This is not the good way
+// }
+
+function prod(num1, num2){
+    return num1 * num2;     // This is the best production ready-way
 }
 
-prod(10 , 20);
+let ans = prod(4, 5);
+// console.log("The result is "+ans);
+
+
+
+// Unlimited Argument access
+
+function addNumbers(){
+    let ans = 0;
+    for(let i=0; i<arguments.length; i++){
+        ans += arguments[i];
+    }
+    return ans;
+}
+
+// Method - 2
+
+function addNumbersV2(...numbers){
+    let ans = 0;
+    for(let i=0; i<numbers.length; i++){
+        ans += numbers[i];
+    }
+    return ans;
+}
+
+let result = addNumbersV2(10,32,45,23,98,100,2);
+console.log(result);
+
